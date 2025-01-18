9cc297dc07f6be4e874e0b6aaa5e23b25354ac02
richard <erichardsv1234@gmail.com>
2025-01-18 20:29:15 -0300

    versao final

--- Sketches/Voltimetro/Voltimetro.ino
+++ Sketches/Voltimetro/Voltimetro.ino
@@ -2,12 +2,12 @@ void setup() {
   // Cria a comunicação serial para exibir os valores no monitor serial
   Serial.begin(115200); // baud = taxa de transferência de bits: bits por segundo
   Serial.println("\n\n\n           Voltímetro 6 canais");
-  Serial.println("A0(V)   A1(V)   A2(V)   A3(V)   A4(V)   A5(V)");
+  Serial.println("A0(V)   A1(V)   A2(V)   A3(V)   A4(V)");
 }
 
 void loop() {
-  float tensao[6];
-  int chMax=5;
+  float tensao[5];
+  int chMax=4;
   int chN;  
   // Mede o valor de 0 a 1023 e converte para tensão
   //           o (float) faz um coerção, pois o comando analogRead retorna um numero inteiro e a tensão é um float
